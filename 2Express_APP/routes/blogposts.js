fb9c8341a58571460882b039cc8d617708418018
SupremePrimeRick <alfredfredrick41@gmail.com>
2023-06-22 18:36:39 -0500

    more routes added

--- 2Express_APP/routes/blogposts.js
+++ 2Express_APP/routes/blogposts.js
@@ -3,29 +3,9 @@ const path = require('path')
 const BlogPost = require('../models/BlogPost')
 const router = express()
 
-router.post('/posts/store', (req, res) => {
-  // models creates a new doc with browser data
-  const createBlogPost = (data) => {
-    return new Promise((resolve, reject) => {
-      BlogPost.create(data, (error, blogpost) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(blogpost)
-        }
-      })
-    })
-  }
-
-  // Usage
-  createBlogPost(req.body)
-    .then((blogpost) => {
-      res.redirect('/')
-    })
-    .catch((error) => {
-      // Handle the error
-      console.error(error)
-    })
+router.post('/posts/store', async (req, res) => {
+  await BlogPost.create(req.body)
+  res.redirect('/')
 })
 
 module.exports = router
